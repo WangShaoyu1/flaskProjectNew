d13a03baa28502ff7fa903afe626446ec241cc49
wy <18672293959@126.com>
2024-09-01 09:59:27 +0800

    update：1、提交一些内容

---
+++ utils/xuan_AI/ai_16/qa_ragas/new_qa_ragas.py
@@ -0,0 +1,4 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+from langchain.output_parsers import StrOutputParser
